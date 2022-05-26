9906a9dbd7faf3f3e86f2d7a7b0de34538d17ca6
Paulo Rodrigo <paulorodrigo1992@gmail.com>
2022-05-26 10:58:56 -0300

    Update department

--- src/main/java/com/example/workshojavafxjdbc/DepartmentListController.java
+++ src/main/java/com/example/workshojavafxjdbc/DepartmentListController.java
@@ -5,6 +5,7 @@ import com.example.workshojavafxjdbc.model.entities.Department;
 import com.example.workshojavafxjdbc.services.DepartmentService;
 import com.example.workshojavafxjdbc.util.Alerts;
 import com.example.workshojavafxjdbc.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -12,10 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
@@ -38,6 +36,9 @@ public class DepartmentListController implements Initializable, DataChangeListen
 
     @FXML
     private TableColumn<Department, String> tableColumnName;
+
+    @FXML
+    private TableColumn<Department, Department> tableColumnEDIT;
 
     @FXML
     private Button btNew;
@@ -76,6 +77,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
         List<Department> list = service.findAll();
         obsList = FXCollections.observableArrayList(list);
         tableViewDepartment.setItems(obsList);
+        initEditButtons();
     }
 
     private void createDialogForm(Department obj, String absoluteName, Stage parentStage){
@@ -102,6 +104,28 @@ public class DepartmentListController implements Initializable, DataChangeListen
         }catch (IOException e){
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
         }
+    }
+
+    private void initEditButtons() {
+        tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnEDIT.setCellFactory(param -> new TableCell<Department, Department>() {
+            private final Button button = new Button("edit");
+
+            @Override
+            protected void updateItem(Department obj, boolean empty) {
+                super.updateItem(obj, empty);
+
+                if (obj == null) {
+                    setGraphic(null);
+                    return;
+                }
+
+                setGraphic(button);
+                button.setOnAction(
+                        event -> createDialogForm(
+                                obj, "DepartmentForm.fxml", Utils.currentStage(event)));
+            }
+        });
     }
 
     @Override
